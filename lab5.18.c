4813f85d90573183409bbdf87868aacd8ceb8c8b
ByBJluk <59995974+ByBJluk@users.noreply.github.com>
2020-01-18 03:26:50 +0500

    update lab5

--- lab5.18.c
+++ lab5.18.c
@@ -9,7 +9,7 @@ int main()
 	int a[15][15]={0}, i, s, j, kp, m, n;
 	
 	while(system("pause"),fflush(stdin),system("cls"),printf("введите размер матрицы(m*n) : "),scanf("%i%i",&m,&n)!=2||n>15||n<=1||m>15||m<=1)
-	printf("Ошибка! некорректный ввод\n");
+		printf("Ошибка! некорректный ввод\n");
 	
 	for(j=0;j<m;j++)
 		for (i=0;i<n;i++)
@@ -26,30 +26,28 @@ int main()
 	
 	for(j=0;j<m;j++)
 		for (i=0;i<n;i++)
-			if(a[j][i]==1)
+			if(a[j][i]==1 && a[j-1][i]==0 && a[j][i-1]==0)
 			{
 				int kj, ki, i1, i2, j2 ,j1 , k1, i3, j3;
 				kj=0;
 				ki=0;
 				i1=i;
-				i2=i;
 				j1=j;
-				j2=j;
 				
 				for(j1;a[j1][i]==1;j1++);	
 					
 				for(i1;a[j][i1]==1;i1++);
 				
-				for(j2;j2<j1;j2++)
-					for(i2;i2<i1;i2++)
-						if(a[j2][i2]==1)
-							k1++;
-				
 				s=j1*i1;
+				
+				for(j1;j1=>j;j1--)
+					for(i1;i1>=i;i1--)
+						if(a[j1[i1]==1)
+							k1++;
 				
 				if(k1==s)
 					kp++;
-						
+				
 				for(j=0;j<m;printf("\n"),j++)
 					for (i=0;i<n;i++)
 						printf(" %i ",a[j][i]);
