b5e6e8fe106527e434eb7a16e072560ccff9754e
ByBJluk <59995974+ByBJluk@users.noreply.github.com>
2020-01-20 10:28:34 +0500

    20.01.20(10:28)

--- lab5.18.c
+++ lab5.18.c
@@ -33,54 +33,46 @@ int main()
 				int kj, ki, i1, i2, j1, j2;
 				kj=0;
 				ki=0;
-				i1=i;
-				j1=j;
-				
-				printf("\n1s=%i\nj1=%i\ni1=%i",s,j1,i1);
-				
-				for(j1;(a[j1][i]==1) && j1!=m;j1++);
-				for(i1;(a[j][i1]==1) && i1!=n;i1++);
+	
+				for(j1=j;(a[j1][i]==1) && j1!=m;j1++);
+				for(i1=i;(a[j][i1]==1) && i1!=n;i1++);
 				s=(j1-j)*(i1-i);
-
-				printf("\n2s=%i\nj1=%i\ni1=%i",s,j1,i1);
 
 				for(j2=j;j2<j1;j2++)
 					for(i2=i;i2<i1;i2++)
 						if(a[j2][i2]==1)
 							s--;
 				
-				printf("\n3s=%i\nj1=%i\ni1=%i",s,j1,i1);
-				
-				
-				i1=i;
-				j1=j;
-				
-				for(j1;a[j1][i]==1;j1++);
-				for(i1;a[j][i1]==1;i1++);
-				
+				for(j1=j;a[j1][i]==1;j1++);
+				for(i1=i;a[j][i1]==1;i1++);
+				printf("\n0");
 				printf("\n4s=%i\nj1=%i\ni1=%i",s,j1,i1);
 				
-				printf("\n0");
-				
-				j1++;
-				i1++;
+				printf("\nj=%i\ni=%i",j,i);
 				
 				if((i==0) && (j==0))
 					{
 					
 						printf("\n1");
+					
 						for(j1;j1>=j;j1--)
-							if(a[j1][i1+1]!=0)
+							if(a[j1][i1]!=0)
 								break;
-						j1=j;
-						for(j1;a[j1][i]==1;j1++);
+						
+						if(j1==j-1)
+							for(j1=j;a[j1][i]==1;j1++);
+					
 						for(i1;i1>=i;i1--)
-							if(a[j1+1][i1]!=0)
+							if(a[j1][i1]!=0)
 								break;
 
 						if(i1==i-1)
+						{
 							j1=0;
 							i1=0;
+						}
+						
+						printf("\n1s=%i\nj1=%i\ni1=%i",s,j1,i1);
 					}
 					
 				if((i==0) && (j!=0))
@@ -88,22 +80,26 @@ int main()
 						printf("\n2");
 						
 						for(j1;j1>=j;j1--)
-							if(a[j1][i1+1]!=0)
+							if(a[j1][i1]!=0)
 								break;
 					
-						j1=j;
-						for(j1;a[j1][i]==1;j1++);
+						if(j1==j-1)
+							for(j1=j;a[j1][i]==1;j1++);
 					
 						for(i1;i1>=i;i1--)
 							{
 								if(a[j-1][i1]!=0)
 									break;
-								if(a[j1+1][i1]!=0)
+								if(a[j1][i1]!=0)
 									break;
 							}
 						if(i1==i-1)	
+							{
 							j1=0;
-							i1=0;	
+							i1=0;
+							}
+							
+						printf("\n2s=%i\nj1=%i\ni1=%i",s,j1,i1);
 					}
 					
 				if((j==0) && (i!=0))
@@ -114,22 +110,27 @@ int main()
 							{
 								if(a[j1][i-1]!=0)
 									break;
-								if(a[j1][i1+1]!=0)
+								if(a[j1][i1]!=0)
 									break;
 							}
-					
-						j1=j;
-						for(j1;a[j1][i]==1;j1++);
+						
+						if(j1==j-1)
+							for(j1=j;a[j1][i]==1;j1++);
 						
 						for(i1;i1>=i;i1--)
 							{
-								if(a[j1+1][i1]!=0)
+								printf("\n%i",i1);
+								if(a[j1][i1]!=0)
 									break;
 							}
-							
+						
 						if(i1==i-1)	
+						{
 							j1=0;
 							i1=0;
+						}
+							
+								printf("\n3s=%i\nj1=%i\ni1=%i",s,j1,i1);
 					}
 				
 				if((j!=0)&&(i!=0))
@@ -139,30 +140,35 @@ int main()
 							{
 								if(a[j1][i-1]!=0)
 									break;
-								if(a[j1][i1+1]!=0)
+								if(a[j1][i1]!=0)
 									break;
 							}
-						
-						j1=j;
-						for(j1;a[j1][i]==1;j1++);
+							
+						if(j1==j-1)
+							for(j1=j;a[j1][i]==1;j1++);
 					
 						for(i1;i1>=i;i1--)
 							{
 								if(a[j-1][i1]!=0)
 									break;
-								if(a[j1+1][i1]!=0)
+								if(a[j1][i1]!=0)
 									break;
 							}
+
 						if(i1==i-1)
+						{
 							j1=0;
 							i1=0;
 						}
+						}
+						printf("\nj=%i\ni=%i",j,i);
 				
+		
 					
-				printf("\n6s=%i\nj1=%i\ni1=%i",s,j1,i1);
-				
 				if((s==0)&&(i1==0)&&(j1==0))
 					kp++;
+					
+				printf("\n7s=%i\nj1=%i\ni1=%i",s,j1,i1);
 			}
 	
 	printf("\nКол-во  прям-ков : %i\n",kp);
